744b3db93b8af37024a9752020c9b534ba58f8ec
Iain MacDonald <IJMacD@gmail.com>
2018-03-10 14:08:22 +0800

    Support GA tracking

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+
 import App from './App';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const env = runtimeEnv();
+
+if (env.REACT_APP_GA_TRACKING) {
+  const ga = function(){(ga.q = ga.q||[]).push(arguments)};
+  ga.l = +new Date();
+  window.ga = ga;
+  ga('create', env.REACT_APP_GA_TRACKING, 'auto');
+  ga('send', 'pageview');
+
+  const a = document.createElement('script');
+  a.async = true;
+  a.src = 'https://www.google-analytics.com/analytics.js';
+  document.head.appendChild(a);
+}
