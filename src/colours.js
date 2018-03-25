a33cf64b14baa4ebb008f0cadb2ad56c63fb03c2
Iain MacDonald <IJMacD@gmail.com>
2018-03-25 21:26:45 +0800

    Add Additional palettes
    Closes #1

--- src/colours.js
+++ src/colours.js
@@ -1,14 +1,3 @@
-export const brightColours = {
-  "world":          "#FF3434",
-  "nation":         "#FFE534",
-  "business":       "#72FF34",
-  "technology":     "#34FFAA",
-  "sports":         "#34AEFF",
-  "entertainment":  "#6E34FF",
-  "health":         "#FF34E8",
-  "science":        "#AE6EFF",
-};
-
 export const originalColours = {
   "world":          "#9c1f1f",
   "nation":         "#9c891f",
@@ -18,6 +7,28 @@ export const originalColours = {
   "entertainment":  "#421f9c",
   "health":         "#9c1f8b",
   "science":        "#68429c",
+};
+
+export const primaryColours = {
+  "world":          "#FF0000",
+  "nation":         "#FFFF00",
+  "business":       "#40FF00",
+  "technology":     "#00BFFF",
+  "sports":         "#FF00FF",
+  "entertainment":  "#0000FF",
+  "health":         "#FF0080",
+  "science":        "#D8D8D8",
+};
+
+export const rainbow = {
+  "world":          "#FF0000",
+  "nation":         "#FF8000",
+  "business":       "#FFFF00",
+  "technology":     "#00FF00",
+  "entertainment":  "#00FF80",
+  "sports":         "#0000FF",
+  "science":        "#800080",
+  "health":         "#808080",
 };
 
 export default originalColours;
