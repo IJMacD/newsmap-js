c827058ad22fbb9e8dea76e05b9d9dc4330a1e57
Iain MacDonald <ijmacd@gmail.com>
2022-09-26 18:21:55 +0800

    Deploy and use CORS proxy

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const port = process.env.PORT || 8000;
 
 app.use(express.static("static"));
 
-app.use("/api", proxy("https://news.google.com"));
+app.use("/api", proxy("https://news.google.com", {
+    proxyReqOptDecorator(proxyReqOpts) {
+        //  -H 'Sec-Fetch-Dest: empty'   -H 'Sec-Fetch-Mode: cors'   -H 'Sec-Fetch-Site: cross-site'
+        proxyReqOpts.headers['Sec-Fetch-Dest'] = null;
+        proxyReqOpts.headers['Sec-Fetch-Mode'] = null;
+        proxyReqOpts.headers['Sec-Fetch-Site'] = null;
+
+        return proxyReqOpts;
+    },
+    userResHeaderDecorator(headers, userReq, userRes, proxyReq, proxyRes) {
+      // recieves an Object of headers, returns an Object of headers.
+      headers["access-control-allow-origin"] = "https://newsmap.ijmacd.com";
+      return headers;
+    }
+}));
 
 app.listen(port, () => console.log("Listening on port " + port));
