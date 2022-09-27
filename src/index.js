958fb64a2d75a18d5cbe394a0dd2a7dd903cbd31
Iain MacDonald <ijmacd@gmail.com>
2022-09-27 09:51:36 +0800

    Try to purge service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import runtimeEnv from '@mars/heroku-js-runtime-env';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister as unregisterServiceWorker } from './registerServiceWorker';
 
 import 'core-js/fn/array/from';
 import 'core-js/fn/array/includes';
@@ -14,18 +13,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(<ErrorHandler><App /></ErrorHandler>, document.getElementById('root'));
-registerServiceWorker();
+unregisterServiceWorker();
 
-const env = runtimeEnv();
 
-if (env.REACT_APP_GA_TRACKING) {
-  const ga = function(){(ga.q = ga.q||[]).push(arguments)};
-  ga.l = +new Date();
-  window.ga = ga;
-  ga('create', env.REACT_APP_GA_TRACKING, 'auto');
-
-  const a = document.createElement('script');
-  a.async = true;
-  a.src = 'https://www.google-analytics.com/analytics.js';
-  document.head.appendChild(a);
-}
+window.location = "https://newsmap.ijmacd.com";
